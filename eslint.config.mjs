8e24aaf4d189b8d2ce361e97d57c40936d0006a8
raphael <raphaelmiranda.l9v@gmail.com>
2025-05-21 22:52:22 +0800

    updated.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // override rule for specific files that import the Prisma client using require()
+  {
+    files: ["src/**/*.{ts,tsx,js}"], // your source files
+    rules: {
+      "@typescript-eslint/no-require-imports": "error", // default: error everywhere
+    },
+  },
+  {
+    files: ["src/**/prisma.ts"], // or whatever files import from generated/prisma
+    rules: {
+      "@typescript-eslint/no-require-imports": "off", // turn it off in just these files
+    },
+  },
 ];
 
 export default eslintConfig;
